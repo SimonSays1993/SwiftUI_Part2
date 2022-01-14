db3d52c40a121d9975f7cc9b8adc9b32db08b88d
Simon McNeil <simonmcneil062@gmail.com>
2022-01-14 08:24:06 -0500

    Created Progress Circle View Animation

---
+++ SwiftUI_Part2_DesignCode/RingView.swift
@@ -0,0 +1,52 @@
+//
+//  RingView.swift
+//  SwiftUI_Part2_DesignCode
+//
+//  Created by Simon Mcneil on 2022-01-12.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    var color1 = Color.red
+    var color2 = Color.purple
+    var width: CGFloat = 88
+    var height: CGFloat = 88
+    var percent: CGFloat = 56
+    
+    @Binding var showProgressRing: Bool
+    
+    var body: some View {
+        let multiplier = width / 44
+        let progress = 1 - (percent / 100)
+        
+        return ZStack {
+            //Inactive String, the grey circle
+            Circle()
+                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
+                .frame(width: width, height: height)
+            
+            Circle()
+                .trim(from: showProgressRing ? progress : 0.999, to: 1)
+                .stroke(
+                    LinearGradient(gradient: Gradient(colors: [color1, color2]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0))
+                .animation(.easeInOut(duration: 1.0).delay(0.3), value: showProgressRing)
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                .frame(width: width, height: height)
+                .shadow(color: color2.opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
+
+    
+            Text("\(Int(percent))%")
+                .font(.system(size: 14 * multiplier))
+                .fontWeight(.bold)
+        }
+    }
+}
+
+struct RingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RingView(showProgressRing: .constant(true))
+    }
+}
--- SwiftUI_Part2_DesignCode/Views/BottomCardView.swift
+++ SwiftUI_Part2_DesignCode/Views/BottomCardView.swift
@@ -16,15 +16,30 @@ struct BottomCardView: View {
                 .frame(width: 40.0, height: 5.0)
                 .cornerRadius(3.0)
                 .opacity(0.1)
+            
             Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor")
                 .multilineTextAlignment(.center)
                 .font(.subheadline)
                 .lineSpacing(4.0)
             
-            RingView(color1: Color(hue: 0.545, saturation: 1.0, brightness: 1.0),
-                     color2: Color(hue: 0.655, saturation: 1.0, brightness: 0.996),
-                     width: 88, height: 88, percent: 78, show: $show)
-            
+            HStack(spacing: 20.0) {
+                RingView(color1: Color(hue: 0.545, saturation: 1.0, brightness: 1.0),
+                         color2: Color(hue: 0.655, saturation: 1.0, brightness: 0.996),
+                         width: 88, height: 88, percent: 78, showProgressRing: $show)
+                    //.animation(.linear.delay(0.2), value: show)
+                
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("SwiftUI").fontWeight(.bold)
+                    Text("12 of 12 sections completed\n10 hours spent so far")
+                        .font(.footnote)
+                        .foregroundColor(Color.gray)
+                        .lineSpacing(4)
+                }
+                .padding(20)
+                .background(Color.white)
+                .cornerRadius(20)
+                .shadow(color: Color.black.opacity(0.2), radius:  20, x: 0, y: 10)
+            }
             Spacer()
         }
         .padding(.top, 8)
@@ -39,6 +54,6 @@ struct BottomCardView: View {
 
 struct BottomCardView_Previews: PreviewProvider {
     static var previews: some View {
-        BottomCardView(show: .constant(false))
+        BottomCardView(show: .constant(true))
     }
 }
