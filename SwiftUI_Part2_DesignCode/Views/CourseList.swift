751a8d99214e0e7f4677adcaf5faaa9b881139d6
Simon McNeil <simonmcneil062@gmail.com>
2022-01-27 22:42:20 -0500

    Added course detail view

--- SwiftUI_Part2_DesignCode/Views/CourseList.swift
+++ SwiftUI_Part2_DesignCode/Views/CourseList.swift
@@ -1,8 +1,9 @@
 //
 //  CourseList.swift
-//  SwiftUI_Part2_DesignCode
+//  DesignCode
 //
-//  Created by Simon Mcneil on 2022-01-17.
+//  Created by Meng To on 2020-01-07.
+//  Copyright © 2020 Meng To. All rights reserved.
 //
 
 import SwiftUI
@@ -17,6 +18,7 @@ struct CourseList: View {
             Color.black.opacity(active ? 0.5 : 0)
                 .animation(.linear, value: active)
                 .edgesIgnoringSafeArea(.all)
+            
             ScrollView {
                 VStack(spacing: 30) {
                     Text("Courses")
@@ -33,9 +35,9 @@ struct CourseList: View {
                                        activeIndex: $activeIndex,
                                        course: courses[index], index: index)
                                 .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
-                                .opacity(activeIndex != index && active ? 0 : 1)
-                                .scaleEffect(activeIndex != index && active ? 0.5 : 1)
-                                .offset(x: activeIndex != index && active ? screen.width : 0)
+                                .opacity(self.activeIndex != index && self.active ? 0 : 1)
+                                .scaleEffect(self.activeIndex != index && self.active ? 0.5 : 1)
+                                .offset(x: self.activeIndex != index && self.active ? screen.width : 0)
                         }
                         .frame(height: 280)
                         .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
@@ -43,12 +45,17 @@ struct CourseList: View {
                     }
                 }
                 .frame(width: screen.width)
-                .animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0))
+                .animation(.spring(response: 0.6, dampingFraction: 0.9, blendDuration: 0))
             }
-            // This looks good on simulator but making swiftUI preview animation add glitchy.
             //.statusBar(hidden: active ? true : false)
-            .animation(.linear, value: active)
+            //.animation(.linear)
         }
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
     }
 }
 
@@ -57,7 +64,7 @@ struct CourseView: View {
     @Binding var active: Bool
     @Binding var activeIndex: Int
     
-    var course: Course 
+    var course: Course
     var index: Int
     
     var body: some View {
@@ -71,16 +78,13 @@ struct CourseView: View {
                 Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
                 
                 Text("Minimal coding experience required, such as in HTML and CSS. Please note that Xcode 11 and Catalina are essential. Once you get everything installed, it'll get a lot friendlier! I added a bunch of troubleshoots at the end of this page to help you navigate the issues you might encounter.")
-
             }
             .padding(30)
-            .frame(maxWidth: show ? .infinity : screen.width - 60,
-                   maxHeight: show ? .infinity : 280, alignment: .top)
+            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280, alignment: .top)
             .offset(y: show ? 460 : 0)
             .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2),
-                    radius: 20, x: 0, y: 20)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .opacity(show ? 1 : 0)
             
             VStack {
@@ -88,7 +92,7 @@ struct CourseView: View {
                     VStack(alignment: .leading, spacing: 8.0) {
                         Text(course.title)
                             .font(.system(size: 24, weight: .bold))
-                            .foregroundColor(Color.white)
+                            .foregroundColor(.white)
                         Text(course.subtitle)
                             .foregroundColor(Color.white.opacity(0.7))
                     }
@@ -100,7 +104,7 @@ struct CourseView: View {
                         VStack {
                             Image(systemName: "xmark")
                                 .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(Color.white)
+                                .foregroundColor(.white)
                         }
                         .frame(width: 36, height: 36)
                         .background(Color.black)
@@ -113,15 +117,14 @@ struct CourseView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: .infinity)
-                    .frame(height: 140, alignment: .top)
+                    .frame(height: show ? 240 : 140, alignment: .top)
             }
             .padding(show ? 30 : 20)
-            .padding(.top, show ? 30 : 0) //we add more top padding as dealing with safeArea notches when in full screen
-            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
+            .padding(.top, CGFloat(show ? 30 : 0))
+            .frame(maxWidth: CGFloat(show ? .infinity : screen.width - 60), maxHeight: CGFloat(show ? 460 : 280))
             .background(Color(course.color))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color(course.color).opacity(0.3),
-                    radius: 20, x: 0, y: 20)
+            .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
             .onTapGesture {
                 self.show.toggle()
                 self.active.toggle()
@@ -131,21 +134,21 @@ struct CourseView: View {
                     self.activeIndex = -1
                 }
             }
+            
+            if show {
+                CourseDetail(course: course, show: $show, active: $active, activeIndex: $activeIndex)
+                    .background(Color.white)
+                    .animation(nil)
+            }
         }
         .frame(height: show ? screen.height : 280)
-        .animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0), value: show)
+        .animation(.spring(response: 0.6, dampingFraction: 0.9, blendDuration: 0))
         .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct CourseList_Previews: PreviewProvider {
-    static var previews: some View {
-        CourseList()
-    }
-}
-
 struct Course: Identifiable {
-    var id: UUID = UUID()
+    var id = UUID()
     var title: String
     var subtitle: String
     var image: UIImage
